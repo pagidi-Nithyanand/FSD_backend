885f668ef5ceb2447a0090b336acb4a25ac9b5ee
Gavini Sumanth <100990200+GaviniSumanth@users.noreply.github.com>
2023-10-20 13:44:21 +0000

    style: eslint support for jest tests

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -8,26 +8,26 @@ describe('POST /user/getGeneratedName', () => {
   })
   it('should return a username of string type', async () => {
     const response = await request(baseURL).post('/user/getGeneratedName')
-    username = response.body
-    expect(username && typeof username == 'string').toBe(true)
+    const username = response.body
+    expect(username && typeof username === 'string').toBe(true)
   })
   it('should return a username with atleast 8 characters', async () => {
     for (let index = 0; index < 50; index++) {
       const response = await request(baseURL).post('/user/getGeneratedName')
-      username = response.body.toString()
+      const username = response.body.toString()
       expect(username.length >= 8).toBe(true)
     }
   })
   it.skip('should return a username with atmost 32 characters', async () => {
     for (let index = 0; index < 50; index++) {
       const response = await request(baseURL).post('/user/getGeneratedName')
-      username = response.body.toString()
+      const username = response.body.toString()
       expect(username.length <= 32).toBe(true)
     }
   })
   it('should return a username with alphanumeric characters', async () => {
     const response = await request(baseURL).post('/user/getGeneratedName')
-    username = response.body
+    const username = response.body
     expect(/^(\w|\d)*$/.test(username)).toBe(true)
   })
 })
