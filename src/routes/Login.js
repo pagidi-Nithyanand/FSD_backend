d7db48361f06c5a528ac532e7f9246fe17b8e6d5
Nithyanand <paigidinithyanand@gmail.com>
2023-11-24 13:38:41 +0530

    style: lint errors fix

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,39 +1,39 @@
-const express = require("express");
-const UserModel = require("../models/UserModel");
-const jwt = require("jsonwebtoken");
-app = express();
-app.post("/login", async (req, res) => {
+const express = require('express')
+const UserModel = require('../models/UserModel')
+const jwt = require('jsonwebtoken')
+app = express()
+app.post('/login', async (req, res) => {
   try {
-    const { username, hash } = req.body;
-    console.log(req.body);
-    let exist = await UserModel.findOne({ username: req.body.username });
-    console.log(exist);
+    const { username, hash } = req.body
+    console.log(req.body)
+    let exist = await UserModel.findOne({ username: req.body.username })
+    console.log(exist)
     if (!exist) {
-      res.send("user doesn't exist");
+      res.send("user doesn't exist")
     } else {
       if (exist.hash === req.body.password) {
-        console.log(exist.id);
+        console.log(exist.id)
         let payload = {
           user: {
-            id: exist.id,
-          },
-        };
+            id: exist.id
+          }
+        }
         await jwt.sign(
           payload,
-          "jwtsecret",
+          'jwtsecret',
           { expiresIn: 3600000 },
           (err, token) => {
-            if (err) throw err;
-            return res.json(token);
+            if (err) throw err
+            return res.json(token)
           }
-        );
+        )
       } else {
-        res.send("incoorect password");
+        res.send('incorrect password')
       }
     }
   } catch (err) {
-    console.log(err);
-    res.send("Server Error");
+    console.log(err)
+    res.send('Server Error')
   }
-});
-module.exports = app;
+})
+module.exports = app
